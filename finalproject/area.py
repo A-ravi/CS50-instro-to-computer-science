32815dbe240ae85dfa83fb85e7a8451369b1ab8a
ravi_10 <rveditz786@gmail.com>
2020-08-08 19:30:58 +0530

    Added some files in final project

---
+++ finalproject/area.py
@@ -0,0 +1,11 @@
+'''
+Write a  program to calculate the area of rectangle..
+'''
+#
+
+height = 10
+width = 20
+area = height * width
+perimeter = (height + width) * 2
+print(area)
+print(perimeter)
